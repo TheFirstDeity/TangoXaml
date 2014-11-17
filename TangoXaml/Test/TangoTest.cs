07d4be124f0ea940dc1dba85b150d3ac11ef4a4f
Devin <me@monadone.com>
2014-11-16 19:37:31 -0700

    Added xUnit test project. Enabled NuGet package restore. Set build action for xaml files to "Page" so that they're compiled into baml.

---
+++ TangoXaml/Test/TangoTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TangoXaml
+{
+    public class TangoTest
+    {
+        internal const string _ManifestResourceName = "TangoXaml.g.resources";
+
+        [Fact]
+        public void CheckBaml()
+        {
+            var assembly = typeof(Actions).Assembly;
+            var resources = new Dictionary<string, object>();
+
+            using (var reader = new ResourceReader(assembly.GetManifestResourceStream(_ManifestResourceName)))
+            {
+                foreach (DictionaryEntry r in reader)
+                {
+                    resources.Add((string)r.Key, r.Value.GetType().Name);
+                }
+            }
+
+            File.WriteAllLines("resources.txt", resources.Keys);
+        }
+    }
+}
